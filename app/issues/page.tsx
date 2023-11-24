f8449e467512957d0ab4be64a632488216c784ee
turtledev <turtledev65@gmail.com>
2023-11-24 19:13:02 +0200

    Show the issues in a table

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,44 @@
-import { Button } from "@radix-ui/themes";
+import prisma from "@/prisma/client";
+import { Button, Table, TableRow } from "@radix-ui/themes";
 import Link from "next/link";
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
+
   return (
     <div>
-      <Button>
+      <Button mb="5">
         <Link href="/issues/new">New Issue</Link>
       </Button>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map(issue => (
+            <TableRow key={issue.id}>
+              <Table.RowHeaderCell>
+                {issue.title}
+                <div className="block md:hidden">{issue.status}</div>
+              </Table.RowHeaderCell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.status}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </TableRow>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 };
