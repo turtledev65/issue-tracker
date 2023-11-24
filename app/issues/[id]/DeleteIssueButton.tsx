91c984fee26b408af9ef78fcd984eec295778526
turtledev <turtledev65@gmail.com>
2023-11-25 01:31:13 +0200

    Delete an issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 import { FaRegTrashAlt } from "react-icons/fa";
 
 const DeleteIssueButton = ({ issueId }: { issueId: string }) => {
+  const router = useRouter();
+
   return (
     <AlertDialog.Root>
       <AlertDialog.Trigger>
@@ -25,7 +28,19 @@ const DeleteIssueButton = ({ issueId }: { issueId: string }) => {
             </Button>
           </AlertDialog.Cancel>
           <AlertDialog.Action>
-            <Button color="red">Delete Issue</Button>
+            <Button
+              color="red"
+              onClick={async () => {
+                await fetch(`/api/issues/${issueId}`, {
+                  method: "DELETE",
+                  headers: { "Content-Type": "application/json" }
+                });
+                router.push("/issues");
+                router.refresh();
+              }}
+            >
+              Delete Issue
+            </Button>
           </AlertDialog.Action>
         </Flex>
       </AlertDialog.Content>
