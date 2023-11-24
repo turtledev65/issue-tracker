ba768a9562b2f913c72ebb7eaec82988abf8e04e
turtledev <turtledev65@gmail.com>
2023-11-24 20:18:58 +0200

    Build a styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,7 @@
 import prisma from "@/prisma/client";
 import { Table, TableRow } from "@radix-ui/themes";
-import Link from "next/link";
 import IssueStatusBadge from "../components/IssueStatusBadge";
+import Link from "../components/Link";
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
