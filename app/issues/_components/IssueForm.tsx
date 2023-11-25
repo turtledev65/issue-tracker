005628694bc9a78c8e8d450b9506b1becd278da5
turtledev <turtledev65@gmail.com>
2023-11-25 12:37:46 +0200

    Add select status button

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -4,8 +4,16 @@ import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 import { issueSchema } from "@/app/validationSchemas";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Issue } from "@prisma/client";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Issue, Status } from "@prisma/client";
+import {
+  Box,
+  Button,
+  Callout,
+  Flex,
+  Grid,
+  Select,
+  TextField
+} from "@radix-ui/themes";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -18,6 +26,15 @@ type IssueForm = z.infer<typeof issueSchema>;
 interface Props {
   issue?: Issue;
 }
+
+const statusMap: Record<
+  Status,
+  { label: string; color: "red" | "yellow" | "green" }
+> = {
+  OPEN: { label: "Open", color: "red" },
+  IN_PROGRESS: { label: "In Progress", color: "yellow" },
+  CLOSED: { label: "Closed", color: "green" }
+};
 
 const IssueForm = ({ issue }: Props) => {
   const router = useRouter();
@@ -59,34 +76,55 @@ const IssueForm = ({ issue }: Props) => {
   });
 
   return (
-    <div className="max-w-xl ">
+    <div>
       {error && (
         <Callout.Root color="red" className="mb-5">
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
       <form className="space-y-3" onSubmit={handleSubmitIssue}>
-        <ErrorMessage>{errors.title?.message}</ErrorMessage>
-        <TextField.Root>
-          <TextField.Input
-            placeholder="Title"
-            defaultValue={issue?.title}
-            {...register("title")}
-          />
-        </TextField.Root>
-        <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Controller
-          name="description"
-          control={control}
-          defaultValue={issue?.description}
-          render={({ field }) => (
-            <SimpleMDE placeholder="Description" {...field} />
-          )}
-        />
-        <Button disabled={isSubmitting}>
-          {issue ? "Update issue" : "Submit issue"}
-          {isSubmitting && <Spinner />}
-        </Button>
+        <Grid columns={{ initial: "1", md: "5" }} gap="5">
+          <Box className="lg:col-span-4">
+            <ErrorMessage>{errors.title?.message}</ErrorMessage>
+            <TextField.Root>
+              <TextField.Input
+                placeholder="Title"
+                defaultValue={issue?.title}
+                {...register("title")}
+              />
+            </TextField.Root>
+            <ErrorMessage>{errors.description?.message}</ErrorMessage>
+            <Controller
+              name="description"
+              control={control}
+              defaultValue={issue?.description}
+              render={({ field }) => (
+                <SimpleMDE placeholder="Description" {...field} />
+              )}
+            />
+          </Box>
+          <Flex direction="column" gap="2" width="100%">
+            <Button disabled={isSubmitting} className="w-full">
+              {issue ? "Update issue" : "Submit issue"}
+              {isSubmitting && <Spinner />}
+            </Button>
+            {issue && (
+              <Select.Root defaultValue={issue.status}>
+                <Select.Trigger />
+                <Select.Content>
+                  <Select.Group>
+                    <Select.Label>Status</Select.Label>
+                    {Object.entries(statusMap).map(([key, value]) => (
+                      <Select.Item value={key} key={key}>
+                        {value.label}
+                      </Select.Item>
+                    ))}
+                  </Select.Group>
+                </Select.Content>
+              </Select.Root>
+            )}
+          </Flex>
+        </Grid>
       </form>
     </div>
   );
