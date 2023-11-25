1f9ec6d97e04b57074c13ea8b76bbb15fab7025a
turtledev <turtledev65@gmail.com>
2023-11-25 02:50:21 +0200

    Improve the user experience

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Spinner from "@/app/components/Spinner";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -8,18 +9,21 @@ import { FaRegTrashAlt } from "react-icons/fa";
 const DeleteIssueButton = ({ issueId }: { issueId: string }) => {
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [deleting, setDeleting] = useState(false);
 
-  const handleDelete = async () => {
+  const handleDeleteIssue = async () => {
     try {
       const res = await fetch(`/api/issues/${issueId}`, {
         method: "DELETE",
         headers: { "Content-Type": "application/json" }
       });
+      setDeleting(true);
       if (!res.ok) throw res;
 
       router.push("/issues");
       router.refresh();
     } catch (error) {
+      setDeleting(false);
       setError(true);
     }
   };
@@ -28,9 +32,10 @@ const DeleteIssueButton = ({ issueId }: { issueId: string }) => {
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="red">
+          <Button color="red" disabled={deleting}>
             <FaRegTrashAlt />
             Delete Issue
+            {deleting && <Spinner />}
           </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content>
@@ -46,7 +51,7 @@ const DeleteIssueButton = ({ issueId }: { issueId: string }) => {
               </Button>
             </AlertDialog.Cancel>
             <AlertDialog.Action>
-              <Button color="red" onClick={handleDelete}>
+              <Button color="red" onClick={handleDeleteIssue}>
                 Delete Issue
               </Button>
             </AlertDialog.Action>
