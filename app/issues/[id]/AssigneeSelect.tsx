3930fd3ff17f793e460656a665ee4a64ba79581b
turtledev <turtledev65@gmail.com>
2023-11-25 20:22:45 +0200

    Refactor AsigneeSelect component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -7,32 +7,24 @@ import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get<User[]>("/api/users").then(res => res.data),
-    staleTime: 60 * 1000, // 60s
-    retry: 3
-  });
-
+  const { data: users, error, isLoading } = useUsers();
   if (isLoading) return <Skeleton height="2rem" />;
   if (error) return null;
+
+  const assignIssue = (userId: string) => {
+    axios
+      .patch(`/api/issues/${issue.id}`, {
+        assignedToUserId: userId.trim() || null
+      })
+      .catch(() => toast.error("Changes could not be saved."));
+  };
 
   return (
     <>
       <Toaster />
       <Select.Root
         defaultValue={issue.assignedToUserId || " "}
-        onValueChange={userId => {
-          axios
-            .patch(`/api/issues/${issue.id}`, {
-              assignedToUserId: userId.trim() || null
-            })
-            .catch(() => toast.error("Changes could not be saved."));
-        }}
+        onValueChange={assignIssue}
       >
         <Select.Trigger placeholder="Assign..." />
         <Select.Content>
@@ -50,5 +42,12 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
+
+const useUsers = () => useQuery<User[]>({
+  queryKey: ["users"],
+  queryFn: () => axios.get<User[]>("/api/users").then(res => res.data),
+  staleTime: 60 * 1000, // 60s
+  retry: 3
+});
 
 export default AssigneeSelect;
