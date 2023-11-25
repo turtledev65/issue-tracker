5b04efe63c2289bc01f0a989940314a9e707761c
turtledev <turtledev65@gmail.com>
2023-11-25 20:49:53 +0200

    Filter issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,23 @@
 import prisma from "@/prisma/client";
+import { Status } from "@prisma/client";
 import { Table, TableRow } from "@radix-ui/themes";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import Link from "../components/Link";
 import IssueActions from "./IssueActions";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+interface Props {
+  searchParams: {
+    status: Status;
+  };
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status) ? searchParams.status : undefined
+
+  const issues = await prisma.issue.findMany({
+    where: { status}
+  });
 
   return (
     <div>
