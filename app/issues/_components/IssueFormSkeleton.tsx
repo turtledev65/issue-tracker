e63fdf7328f419cc62015c5dfeb37eea0e2b904b
turtledev <turtledev65@gmail.com>
2023-11-25 12:37:55 +0200

    Update issue form skeleton

--- app/issues/_components/IssueFormSkeleton.tsx
+++ app/issues/_components/IssueFormSkeleton.tsx
@@ -1,12 +1,19 @@
 import Skeleton from "@/app/components/Skeleton";
-import { Box, Button } from "@radix-ui/themes";
+import { Box, Button, Flex, Grid } from "@radix-ui/themes";
 
 const IssueFormSkeleton = () => {
   return (
-    <Box className="max-w-xl ">
-      <Skeleton height="2rem" />
-      <Skeleton height="20rem" />
-      <Button>Submit new issue</Button>
+    <Box>
+      <Grid columns={{ initial: "1", md: "5" }} gap="5">
+        <Box className="lg:col-span-4">
+          <Skeleton height="2rem" />
+          <Skeleton height="20rem" className="mt-3" />
+        </Box>
+        <Flex direction="column" gap="2" width="100%">
+          <Button>...</Button>
+          <Skeleton height="2rem" />
+        </Flex>
+      </Grid>
     </Box>
   );
 };
